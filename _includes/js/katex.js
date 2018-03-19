ee5cc46c0642fbed23f77e036c880c03f87ceb0c
Steven Karas <steven.karas@gmail.com>
2018-03-19 20:56:47 +0200

    render katex that was mangled by GHP's forcing kramdown to use mathjax

--- _includes/js/katex.js
+++ _includes/js/katex.js
@@ -3,10 +3,14 @@
 {% include js/katex.auto-render.min.js %}
 
 document.addEventListener("DOMContentLoaded", function() {
-  renderMathInElement(document.body, {'delimiters' : [
-    {left: "$$", right: "$$", display: true},
-    {left: "\\[", right: "\\]", display: true},
-    {left: "$", right: "$", display: false},
-    {left: "\\(", right: "\\)", display: false}
-  ]});
+    renderMathInElement(document.body, {'delimiters' : [
+        {left: "$$", right: "$$", display: true},
+        {left: "\\[", right: "\\]", display: true},
+        {left: "$", right: "$", display: false},
+        {left: "\\(", right: "\\)", display: false}
+    ]});
+
+    document.querySelectorAll("script[type='math/tex; mode=display']").forEach(function(el) {
+        el.outerHTML = katex.renderToString(el.textContent.replace(/%.*/g, ''), { displayMode: true });
+    });
 });
